c9e8a436cf5fa0985a90afab9bcd48a38f9ffb11
Tobi Ajenjo <tobias.ajenjo@hotmail.com>
2023-12-18 21:32:22 -0300

    angular y spring a la vez

--- angular/Angular---login-main/src/app/auth/login/login.component.ts
+++ angular/Angular---login-main/src/app/auth/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../shared/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,12 +9,13 @@ import { AuthService } from '../../shared/services/auth.service';
 export class LoginComponent {
   credentials = { username: '', password: '' };
 
-  constructor(private authService: AuthService) {}
+  constructor(private router: Router,private authService: AuthService) {}
 
   login(): void {
     this.authService.login(this.credentials).subscribe(
       (response) => {
         this.authService.setToken(response.token);
+		this.router.navigate(['/']);
         // Redirigir a la página principal u otra página después del login
       },
       (error) => {
