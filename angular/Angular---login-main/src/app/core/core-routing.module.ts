ab944dc814b893ecb36cbec6d7893375e40e71ee
Lucio Molina <lucio_molina_365@live.com>
2023-12-22 03:00:50 -0300

    getCrew

--- angular/Angular---login-main/src/app/core/core-routing.module.ts
+++ angular/Angular---login-main/src/app/core/core-routing.module.ts
@@ -5,7 +5,7 @@ import { Routes, RouterModule } from '@angular/router';
 import {CrewComponent } from './menu/pages/crew/crew.component';
 import {MenuComponent } from './menu/menu.component';
 import {CrewListComponent } from './menu/pages/crew-list/crew-list.component';
-
+import {CrewInfoComponent} from "./menu/pages/crew-info/crew-info.component";
 
 const routes: Routes = [
   {
@@ -26,7 +26,15 @@ const routes: Routes = [
       {
         path: '**',
         redirectTo: 'menu'
-      }
+      },
+      { 
+    	  path: 'crew-list', 
+    	  component: CrewListComponent 
+      },
+      { 
+    	  path: 'crew-info/:name',
+    	  component: CrewInfoComponent
+      },
     ]
   }
 ];
